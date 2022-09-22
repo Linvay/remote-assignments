f7bd276de17587de5b8f571229742e4d730b7ed8
Linvay <63941955+Linvay@users.noreply.github.com>
2022-09-22 22:56:30 +0800

    added sum.html

--- Week-2/Assignments/index.js
+++ Week-2/Assignments/index.js
@@ -1,7 +1,9 @@
-// code reference: https://levelup.gitconnected.com/set-up-and-run-a-simple-node-server-project-38b403a3dc09
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile("index.html", {root: __dirname});
